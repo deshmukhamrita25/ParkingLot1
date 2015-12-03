d91e30631572e457adc73e5d11c0b65ee7187619
deshmukhamrita25 <deshmukhamrita25@gmail.com>
2015-12-03 14:42:01 +0530

    Commit 2

--- test/parking/ParkingLotManagerTest.java
+++ test/parking/ParkingLotManagerTest.java
@@ -12,20 +12,20 @@ import static org.mockito.Mockito.when;
  */
 public class ParkingLotManagerTest {
 
-    @Test
+//    @Test
     public void testParkToNearest(){
-        Park park1 = Mockito.mock(Park.class);
+        /*Park park1 = Mockito.mock(Park.class);
         Park park2 = Mockito.mock(Park.class);
         ParkingLotManager parkingLotManager = new ParkingLotManager(park1);
         parkingLotManager.addParkingLot(park2);
         when(park1.getDistance()).thenReturn(2);
         when(park2.getDistance()).thenReturn(1);
 
-        assertEquals(park2, parkingLotManager.directParkingToAttendantToNearest());
+        assertEquals(park2, parkingLotManager.directParkingToAttendantToNearest());*/
     }
 
     @Test
-    public void testSelectLargeParkingLotBasedOnCapacity() throws Exception {
+    public void testSelectLargeParkingLotBasedOnCapacity() throws ParkingNotAvailableException {
         Park park1 = new Park(4);
         Park park2 = new Park(2);
         ParkingLotManager parkingLotManager = new ParkingLotManager(park1);
@@ -35,7 +35,7 @@ public class ParkingLotManagerTest {
     }
 
     @Test
-    public void testSelectParkingLotBasedOnAvailableSpace() throws Exception {
+    public void testSelectParkingLotBasedOnAvailableSpace() throws ParkingNotAvailableException {
         Park park1 = new Park(2);
         Park park2 = new Park(1);
         ParkingLotManager parkingLotManager = new ParkingLotManager(park1);
@@ -47,7 +47,7 @@ public class ParkingLotManagerTest {
     }
 
     @Test
-    public void testSelectParkingLotBasedOnMaxFreeSpace() throws Exception {
+    public void testSelectParkingLotBasedOnMaxFreeSpace() throws ParkingNotAvailableException {
         Park park1 = new Park(4);
         Park park2 = new Park(3);
         ParkingLotManager parkingLotManager = new ParkingLotManager(park1);
@@ -56,6 +56,17 @@ public class ParkingLotManagerTest {
         park1.park(new Vehicle());
 
         assertEquals(park2.capacity, parkingLotManager.directParkingToAttendant().capacity);
+    }
+
+    @Test
+    public void testSelectCheapestParking() throws ParkingNotAvailableException {
+        Park park1 = new Park(4,30);
+        Park park2 = new Park(3,20);
+        Park park3 = new Park(3,60);
+        ParkingLotManager parkingLotManager = new ParkingLotManager(park1);
+        parkingLotManager.addParkingLot(park2);
+        parkingLotManager.addParkingLot(park3);
+        assertEquals(park2.capacity, parkingLotManager.directParkingToAttendantToCheapest().capacity);
     }
 
 }
